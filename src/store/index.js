e33702f80fd4a51ffaa367c302c0e9c806229862
Imre Toth <tothimre@designtech.hu>
2021-03-07 21:46:22 +0100

    Added elements for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,19 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    config: {},
+    categories: [],
+  },
+  getters: {
+    config: (state) => state.config,
+    categories: (state) => state.categories,
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    setConfig(state, config) {
+      state.columns = config;
+    },
+    setCategories(state, categories) {
+      state.categories = categories;
+    },
   },
 });
