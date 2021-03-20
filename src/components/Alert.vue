1621befea849d996fc153827d636bf0d62313d6a
Imre Toth <tothimre@designtech.hu>
2021-03-20 16:50:42 +0100

    Added alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="alert alert-success" v-if="messages.success">
+      {{ messages.success }}
+    </div>
+
+    <div class="alert alert-danger" v-if="messages.error">
+      {{ messages.error }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: {
+    messages: {
+      type: Object,
+    },
+  },
+};
+</script>
